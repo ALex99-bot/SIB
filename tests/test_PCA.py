70f357baf3e670692e5e11f2b85123355c78d080
Pedro Alexandre Gonçalves <72793686+ALex99-bot@users.noreply.github.com>
2021-11-11 11:50:25 +0000

    unittesting correction

--- tests/test_PCA.py
+++ tests/test_PCA.py
@@ -9,7 +9,8 @@ class MyTestCase(unittest.TestCase):
         self.dataset = Dataset.from_data(self.filename, labeled=True)
 
     def test_PCA(self):
-        self.assertEqual(True, False)  # add assertion here
+        pca = PCA(2)
+        self.assertEqual(pca.fit_transform(self.dataset).shape, (209,2))
 
 
 if __name__ == '__main__':
