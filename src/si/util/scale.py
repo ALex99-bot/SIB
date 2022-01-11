fe2bbc6ebdd39fcab8a93937d688adaee1243da3
Pedro Alexandre Gonçalves <72793686+ALex99-bot@users.noreply.github.com>
2022-01-11 11:28:23 +0000

    Changes by dm

--- src/si/util/scale.py
+++ src/si/util/scale.py
@@ -47,7 +47,7 @@ class StandardScaler:
             dataset.X = z
             return dataset
         else:
-            return Dataset(z, copy(dataset.Y), copy(dataset._xnames), copy(dataset._yname))
+            return Dataset(z, copy(dataset.Y), copy(dataset.xnames), copy(dataset.yname))
 
     def fit_transform(self, dataset, inline=False):
         """
